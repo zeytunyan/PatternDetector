0d541bd1a4a43e9b7d1ebc42d2c1c6fa488f0aa3
zeytunyan <georgiy.zeytunyan@gmial.com>
2023-01-17 20:07:16 +0300

    Change project structure

--- Structs/DocumentResult.cs
+++ Structs/DocumentResult.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Collections.Generic;
+
+
+namespace PatternDetector
+{
+    struct DocumentResult
+    {
+        public string ClassName;
+        public List<string> ClassResults;
+
+        public DocumentResult(string className, List<string> classResults)
+        {
+            ClassName = className;
+            ClassResults = classResults;
+        }
+    }
+}
--- Structs/ProjectResult.cs
+++ Structs/ProjectResult.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Collections.Generic;
+
+
+namespace PatternDetector
+{
+    struct ProjectResult
+    {
+        public string DocumentName;
+        public List<DocumentResult> DocumentResults;
+
+        public ProjectResult(string documentName, List<DocumentResult> documentResults)
+        {
+            DocumentName = documentName;
+            DocumentResults = documentResults;
+        }
+    }
+}
